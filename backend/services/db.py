c4f3694f4e2c152995608d27ac906ecabc88fd52
AndjDavis <andi.idavis@gmail.com>
2025-08-19 18:03:21 -0600

    db connections moved to services/db.py

--- backend/services/db.py
+++ backend/services/db.py
@@ -2,3 +2,12 @@ from prisma import Prisma
 
 db = Prisma()
 
+
+def connect_db():
+    if not db.is_connected():
+        db.connect()
+
+
+def disconnect_db():
+    if db.is_connected():
+        db.disconnect()
